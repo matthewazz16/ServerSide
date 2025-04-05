636d30dcf95145caaea1032c7575db099041f2ff
Saviour <matthewazzopardi05@gmail.com>
2025-04-05 13:10:33 +0000

    Adding routes for colleges and students

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CollegeController;
+use App\Http\Controllers\StudentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('colleges', CollegeController::class);
+Route::resource('students', StudentController::class);
